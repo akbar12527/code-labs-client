92888faab134cb16fccfcc57a4b28c24eff41da7
whwowy <akbar12527@gmail.com>
2022-04-06 03:22:53 +0530

    Add Assets, Animated Icons & Project Utitilies, Tailwind Support & Additional Theme presets

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        horizon: {
+          base1: "#003973",
+          base2: "#E5E5BE",
+        },
+        megaTron: {
+          base1: "#C6FFDD",
+          base2: "#FBD786",
+        },
+        summerDog: {
+          base1: "#a8ff78",
+          base2: "#78ffd6",
+        },
+        delicate: {
+          base1: "#D3CCE3",
+          base2: "#E9E4F0",
+        },
+        sublimeLight: {
+          base1: "#FC5C7D",
+          base2: "#6A82FB",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
